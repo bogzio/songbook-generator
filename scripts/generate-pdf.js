a8c4a8ac18a3b35a102c9ca816e5fc87695e556e
bogzio <bogzio@proton.me>
2025-05-03 00:33:28 +0200

    generowanie pdf - czekanie na czcionki

--- scripts/generate-pdf.js
+++ scripts/generate-pdf.js
@@ -18,20 +18,33 @@ const outputDirectory = path.join(path.normalize(songsRepository), Config.output
 
     await page.goto(`file://${path.resolve(path.join(outputDirectory, Config.htmlOutputFile))}`);
 
+    await page.evaluateHandle('document.fonts.ready');
+
     await page.pdf({
         path: path.join(outputDirectory, Config.pdfOutputFile),
         printBackground: true,
         width: '148.5mm',
         height: '210mm',
+        waitForFonts: true,
     });
 
+    await browser.close();
+})();
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
     await page.goto(`file://${path.resolve(path.join(outputDirectory, Config.htmlBookletOutputFile))}`);
+
+    await page.evaluateHandle('document.fonts.ready');
 
     await page.pdf({
         path: path.join(outputDirectory, Config.pdfBookletOutputFile),
         printBackground: true,
         width: '297mm',
         height: '210mm',
+        waitForFonts: true,
     });
 
     await browser.close();
