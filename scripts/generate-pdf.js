0320d198e19d1538747b26b9161d8a44c7b6f70e
bogzio <bogzio@proton.me>
2025-05-05 00:26:01 +0200

    generate-pdf - refactor

--- scripts/generate-pdf.js
+++ scripts/generate-pdf.js
@@ -2,48 +2,38 @@ const path = require('path');
 const puppeteer = require('puppeteer');
 const Config = require('../config.json');
 
+const generateFile = async (inputFile, outputFile, options) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(inputFile);
+    await page.pdf({
+        path: outputFile,
+        printBackground: true,
+        ...options,
+    });
+    await browser.close();
+}
 
 const generatePdf = async (songbookPath) => {
-
     const outputDirectory = path.join(path.normalize(songbookPath), Config.outputDirectory);
-
-    await (async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-
-        await page.goto(`file://${path.resolve(path.join(outputDirectory, Config.htmlOutputFile))}`);
-
-        await page.evaluateHandle('document.fonts.ready');
-
-        await page.pdf({
-            path: path.join(outputDirectory, Config.pdfOutputFile),
-            printBackground: true,
-            width: '148.5mm',
-            height: '210mm',
-            waitForFonts: true,
-        });
-
-        await browser.close();
-    })();
-
-    await (async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-
-        await page.goto(`file://${path.resolve(path.join(outputDirectory, Config.htmlBookletOutputFile))}`);
-
-        await page.evaluateHandle('document.fonts.ready');
-
-        await page.pdf({
-            path: path.join(outputDirectory, Config.pdfBookletOutputFile),
-            printBackground: true,
-            width: '297mm',
-            height: '210mm',
-            waitForFonts: true,
-        });
-
-        await browser.close();
-    })();
+    await Promise.all([
+        generateFile(
+            `file://${path.resolve(path.join(outputDirectory, Config.htmlOutputFile))}`,
+            path.join(outputDirectory, Config.pdfOutputFile),
+            {
+                width: '148.5mm',
+                height: '210mm',
+            },
+        ),
+        generateFile(
+            `file://${path.resolve(path.join(outputDirectory, Config.htmlBookletOutputFile))}`,
+            path.join(outputDirectory, Config.pdfBookletOutputFile),
+            {
+                width: '297mm',
+                height: '210mm',
+            },
+        ),
+    ])
 }
 
 module.exports.generatePdf = generatePdf;
